0f4c16f330d44a9c29b23b64425d3a459ff8c60e
miguepoloc <miguepoloc@gmail.com>
2020-02-25 15:21:25 -0500

    Comienzo react+Django pero con errores

--- boya/urls.py
+++ boya/urls.py
@@ -18,5 +18,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('', include('apps.prueba.urls')),
+    path('links/', include('apps.links.urls')),
     path('admin/', admin.site.urls),
 ]
