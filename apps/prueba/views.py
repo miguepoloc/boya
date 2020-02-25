c406dc86ff541b197878ad945b055614a0692477
miguepoloc <miguepoloc@gmail.com>
2020-02-25 14:02:20 -0500

    Prueba template para valor de temperatura

--- apps/prueba/views.py
+++ apps/prueba/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .helpers import leertemp
 
 def index(request):
-    return HttpResponse("Hey")
+    temp = leertemp
+    return render(request, 'prueba/index.html', {"temp": temp})
+
+# def institucion(request):
+#     datos_institucionales = Institucion.objects.all()
+#     return render(request, 'institucion/institucion.html', {'datos_institucionales': datos_institucionales})
