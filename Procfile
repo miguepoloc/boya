d622c4f7986f51d7ca7e8edf6dbffd9ed4574c14
miguepoloc <miguepoloc@gmail.com>
2020-04-21 12:57:43 -0500

    configuracion Heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn boya.wsgi --log-file -
--- apps/api/models.py
+++ apps/api/models.py
@@ -6,6 +6,10 @@ class Temperatura(models.Model):
     id = models.AutoField(primary_key=True)
     temperatura = models.FloatField()
     fecha = models.CharField(max_length=200, default=timezone.now)
+    unidad = models.CharField(max_length=10)
+    flag = models.IntegerField()
+    latitud = models.CharField(max_length=100)
+    longitud = models.CharField(max_length=100)
 
     def __str__(self):
         return str(self.id)
